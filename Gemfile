c914129ff8f1078bfe0bbc9a9a4499f3fc441b1a
Anirudhan <rajegannathan@gmail.com>
2013-12-26 00:07:32 -0800

    deployment setup for qa environment

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source 'http://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
 
+# 12factor app
+gem 'dotenv-rails'
+
 # Use mysql as the database for Active Record
 gem 'mysql2'
 
@@ -22,7 +25,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+# gem 'turbolinks'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
@@ -36,16 +39,16 @@ end
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
 # Use unicorn as the app server
-# gem 'unicorn'
-# gem 'unicorn-rails'
+gem 'unicorn'
 
 # Use Capistrano for deployment
-# gem 'capistrano', group: :development
+gem 'capistrano', group: :development
+gem 'capistrano-rails', group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 #
-gem 'capistrano'
+
 gem 'sidekiq'
 gem 'bootstrap-sass', '~> 3.0.2.0'
 gem 'font-awesome-rails'
---
+++ config/deploy.rb
@@ -0,0 +1,58 @@
+set :application, 'wikipedia_scrapper'
+set :repo_url, 'git@github.com:rajegannathan/wikipedia-scrapper.git'
+
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :deploy_to, '/var/webapps/wikipedia-scrapper'
+set :scm, :git
+
+set :ssh_options, {
+  forward_agent: true
+}
+
+# set :format, :pretty
+# set :log_level, :debug
+# set :pty, true
+
+set :linked_files, %w{config/database.yml .env}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :unicorn_pid_file, 'tmp/pids/unicorn.pid'
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+
+      if test "[ -f #{current_path}/#{fetch(:unicorn_pid_file)} ]" then
+        old_unicorn_pid = capture "cat #{current_path}/#{fetch(:unicorn_pid_file)}"
+        if test "kill -0 #{old_unicorn_pid} > /dev/null 2>&1 " then
+          execute "kill -USR2 #{old_unicorn_pid}"
+        else
+          execute "unicorn -c #{current_path}/config/unicorn.rb -E #{fetch(:stage)} -D"
+        end
+      else
+        execute "unicorn -c #{current_path}/config/unicorn.rb -E #{fetch(:stage)} -D"
+      end
+
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
