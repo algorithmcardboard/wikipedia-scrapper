1ca4e288b5a99edc6ca874d3c2fa033d4836ed2d
Anirudhan <rajegannathan@gmail.com>
2013-12-26 08:15:09 -0800

    trying to find the source of angular injector issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require 'wikiscrapper'
 //= require 'bootstrap'
+//= require 'wikiscrapper'
 //= require_tree .
